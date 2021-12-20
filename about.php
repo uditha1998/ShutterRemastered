cbb191952f9f3b94f095463648d420bdec3f7a80
hirusha2577 <56668123+hirusha2577@users.noreply.github.com>
2021-12-20 23:49:46 +0530

    #3 create about page

---
+++ about.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+
+<body>
+
+    <?php
+    include './include/header.php';
+    ?>
+    <?php
+    include './include/banner.php';
+    ?>
+
+    <div class="about_contener">
+
+        <div class="about_contener_r1">
+            <img class="about_contener_r1_image1" src="./image/login.jpg" alt="">
+            <img class="about_contener_r1_image2" src="./image/about.jpg" alt="">
+            <div class="about_contener_r1_dis">
+                <h1>About us</h1>
+                <p>We provide Event Photography Management System
+                    for customers and photographers.Anyone can refer our website.
+                    After the login customer become to the client.
+                    only the client can hire photographer for any chosen event.</p>
+
+                <h1>Our vision</h1>
+                <p> Our vision is to make a market place to all the
+                    photographers and customers from the each and every
+                    area of the country </p>
+
+                <h1>Our mission</h1>
+                <p>Our mission is to set up virtual infrastructure
+                    which help to showcase the talent of photographers.
+                    On the other hand,client will be able to hire a photographer using fingertips easily. </p>
+            </div>
+
+        </div>
+
+       
+
+    </div>
+
+
+
+    <?php
+    include './include/footer.php';
+    ?>
+
+    <script src="./js/script.js"></script>
+</body>
+
+</html>
